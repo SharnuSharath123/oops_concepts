364d026c0ecba852f951c069a0b394bf7cdd6a8f
SharnuSharath123 <sharnuburli143@gmail.com>
2024-06-25 23:58:48 +0530

    constructor

---
+++ constructor.py
@@ -0,0 +1,42 @@
+# how constructors are working:
+
+class MyClass:
+    def __init__(self):
+        print("This is constructor...")
+
+    def m1(self):
+        print("Hello!!!")
+
+mc = MyClass()  # invoke constructor automatically
+mc.m1()   # method we have call explicitly by using object
+
+
+
+# example for constructor have take argument
+
+class myClass:
+    name = "Moni"
+    def __init__(self, name):  # constructor expecting one argument
+        print(name)
+        print(self.name)
+
+mc = MyClass("David")  # passing parameter to the constructor
+
+
+
+# example for class employee  
+
+class Emp:
+    def __init__(self, eid, ename, sal):
+        self.eid = eid
+        self.ename = ename 
+        self.sal = sal
+
+    def display(self):
+        print(self.eid, self.ename, self.sal)
+
+e1 = Emp(101, "bash", 50000)
+e1.display()
+
+e2 = Emp(102, "bruce", 60000)
+e2.display()
